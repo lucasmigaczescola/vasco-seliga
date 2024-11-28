0189e4cbc41b62997f40d7407e2d9bee0bf0002e
lucasmigaczescola <141263921+lucasmigaczescola@users.noreply.github.com>
2024-11-28 20:01:07 -0300

    main commi

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('motivacao-button').addEventListener('click', function() {
+    document.getElementById('motivacao-message').textContent = '"Lá no alto de tantas glórias. Brilhou, brilhou, um novo sol. Clareando com seus raios verde e branco."';
+});
